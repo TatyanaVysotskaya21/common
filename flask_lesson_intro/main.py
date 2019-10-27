cc9e1537880b50e867c7e3508b627165420472ff
TatyanaVysotskaya21 <tatiana.vysozkaya@gmail.com>
2019-10-27 19:01:48 +0200

    H#W_8_flask

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -9,5 +9,40 @@ def get_home_page():
     return render_template("home.html", data=get_data())
 
 
+@app.route('/alarm_clock')
+def get_alarm_clock():
+    return render_template("alarm_clock.html", data=get_data())
+
+
+@app.route('/headphones')
+def get_headphones():
+    return render_template("headphones.html", data=get_data())
+
+
+@app.route('/iPod')
+def get_iPod():
+    return render_template("iPod.html", data=get_data())
+
+
+@app.route('/calculator')
+def get_calculator():
+    return render_template("calculator.html", data=get_data())
+
+
+@app.route('/coffeemaker')
+def get_coffeemaker():
+    return render_template("coffeemaker.html", data=get_data())
+
+
+@app.route('/battery_charger')
+def get_battery_charger():
+    return render_template("battery_charger.html", data=get_data())
+
+
+@app.route('/author')
+def get_author():
+    return render_template("author.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
